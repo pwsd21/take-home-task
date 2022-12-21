defcd39455823bc2a711e5062c8e266436e4b62b
pwsd21 <pawansachdeva1998@gmail.com>
2022-12-21 15:55:40 +0530

    Added sorting functionality in table

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,29 @@
 export const PAGE_SIZE = 5;
 export const ITEM_PER_PAGE = 30;
+
+export const OPTION_DATA = [
+  {
+    id: 0,
+    value: "None",
+  },
+  {
+    id: 1,
+    value: "Name",
+  },
+  {
+    id: 2,
+    value: "City",
+  },
+  {
+    id: 3,
+    value: "Abbreviation",
+  },
+  {
+    id: 4,
+    value: "Conference",
+  },
+  {
+    id: 5,
+    value: "Division",
+  },
+];
